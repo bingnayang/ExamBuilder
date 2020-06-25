3e6fce6a8dc53b2355706ba6e21321bc5aa233f0
BingNa Yang <bingnayang@gmail.com>
2020-06-25 10:25:45 -0400

    check if the xml already existed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,44 +9,60 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 
 public class Main {
 
     public static void main(String[] args) {
-//        createXMLFile();
+        try {
+            // Check if ExamBuilder.xml file existed, if not create the xml file
+            File fileTesting = new File("/Users/Bing/Documents/GitHub/BingnaYang.github.io/ExamBuilder/src/com/company/ExamBuilder.xml");
+            if(fileTesting.exists()){
+                System.out.println("XML file already exist");
+            }else{
+                System.out.println("XML file creating....");
+                createXMLFile();
+                System.out.println("ExamBuilder XML file successfully created");
+            }
 
-        List<Exam> examsList = new ArrayList<>();
-        List<String> option = new ArrayList<>();
 
-        int id_1 = 0;
-        String q1 = "True/False: The base 2 logarithm of 100 is 2";
-        String option1_1 = "True";
-        String option1_2 = "False";
-        String answer = "FALSE";
-        option.add(option1_1);
-        option.add(option1_2);
-        examsList.add(new Exam(id_1,q1,option,answer));
 
-        List<String> option1 = new ArrayList<>();
-        int id_2 = 1;
-        String q2 = "Big O notation tells";
-        String option2_1 = "how the speed of an algorithm relates to the number of items.";
-        String option2_2 = "the running time of an algorithm for a given size data structure.";
-        String option2_3 = "the running time of an algorithm for a given number of items.";
-        String option2_4 = "how the size of a data structure relates to the number of items.";
-        String answer2 = "A";
-        option1.add(option2_1);
-        option1.add(option2_2);
-        option1.add(option2_3);
-        option1.add(option2_4);
+            
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
-        examsList.add(new Exam(id_2,q2,option1,answer2));
-        //-----------------------------------------------
 
-        addQuestionToXML(examsList);
+//        List<Exam> examsList = new ArrayList<>();
+//
+//        List<String> option = new ArrayList<>();
+//        int id_1 = 0;
+//        String q1 = "True/False: The base 2 logarithm of 100 is 2";
+//        String option1_1 = "True";
+//        String option1_2 = "False";
+//        String answer = "FALSE";
+//        option.add(option1_1);
+//        option.add(option1_2);
+//        examsList.add(new Exam(id_1,q1,option,answer));
+//
+//        List<String> option1 = new ArrayList<>();
+//        int id_2 = 1;
+//        String q2 = "Big O notation tells";
+//        String option2_1 = "how the speed of an algorithm relates to the number of items.";
+//        String option2_2 = "the running time of an algorithm for a given size data structure.";
+//        String option2_3 = "the running time of an algorithm for a given number of items.";
+//        String option2_4 = "how the size of a data structure relates to the number of items.";
+//        String answer2 = "A";
+//        option1.add(option2_1);
+//        option1.add(option2_2);
+//        option1.add(option2_3);
+//        option1.add(option2_4);
+//
+//        examsList.add(new Exam(id_2,q2,option1,answer2));
+//        //-----------------------------------------------
+//
+//        addQuestionToXML(examsList);
 
     }
     public static void createXMLFile(){
@@ -73,7 +89,6 @@ public class Main {
 
     public static void addQuestionToXML(List<Exam> exams){
         try {
-
             DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
             Document document = documentBuilder.parse("/Users/Bing/Documents/GitHub/BingnaYang.github.io/ExamBuilder/src/com/company/ExamBuilder.xml");
@@ -101,10 +116,9 @@ public class Main {
                 Element answer = document.createElement("answer");
                 answer.appendChild(document.createTextNode(examList.getAnswer()));
                 detail.appendChild(answer);
-
+                // Append all elements to the root element
                 root.appendChild(detail);
             }
-
 
             DOMSource source = new DOMSource(document);
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
